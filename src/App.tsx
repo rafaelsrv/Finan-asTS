afe0532f1253a1dbbec4f2da54fefed70d2641d3
RafaelSRV <rafael-srv@hotmail.com>
2023-02-27 14:03:11 -0300

    Subindo tipagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import * as C from './App.styles' 
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { items } from './data/items';
+import { categories } from './data/categories';
 
 const App = () => {
+  const [list, setList] = useState(items);
 
   return (  
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
-        <C.Body>
-          ...
-        </C.Body>
-      </C.Header>
+      </C.Header>  
+      <C.Body>
+          
+          {/* Área de Informações*/}
+
+          {/* Área de Inserção*/}
+
+          {/* Tabela de Itens*/}
+
+      </C.Body>
+      
     </C.Container>
   );
 }
